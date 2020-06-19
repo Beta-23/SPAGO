ca3030578bb46e1a7b16fc8909fb3f8e0af97bbf
Beta-23 <alfred.dominguez@gmail.com>
2020-06-18 22:55:56 -0700

    Change Cron fetch interval to a minute

--- worker/index.js
+++ worker/index.js
@@ -1,5 +1,7 @@
 var CronJob = require('cron').CronJob;
-var job = new CronJob('* * * * * *', function() {
+var job = new CronJob('* /1 * * * *', function() {
   console.log('You will see this message every second');
 }, null, true, 'America/Los_Angeles');
 job.start();
+
+
