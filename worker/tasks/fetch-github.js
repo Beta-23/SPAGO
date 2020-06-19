424d27ee50f018e8f03164446e27eab0848c4591
Beta-23 <alfred.dominguez@gmail.com>
2020-06-18 19:26:01 -0700

    Test fetch of json file

---
+++ worker/tasks/fetch-github.js
@@ -0,0 +1,12 @@
+const fetch = require('node-fetch');
+
+// JSON data
+const baseURL = 'https://jobs.github.com/positions.json'
+
+module.exports = async function fetchGithub () {
+    const res = await fetch(baseURL);
+    const jobs = await res.json();
+    console.log(jobs.length);
+}
+
+module.exports();
